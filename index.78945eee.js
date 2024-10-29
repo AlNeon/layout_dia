54c2f99de35097ea1553e611f1fd009ea7738ac5
Oleksii Karpov <alneon1@gmail.com>
2024-10-29 12:23:52 +0200

    make build

---
+++ index.78945eee.js
@@ -0,0 +1,2 @@
+const e=document.querySelectorAll(".header__image"),t=document.getElementById("leftArrow"),c=document.getElementById("rightArrow");let n=0;function a(){e.forEach((e,t)=>{e.classList.remove("header__image--active"),t===n&&e.classList.add("header__image--active")})}function d(t){n=(n+t+e.length)%e.length,a()}t.addEventListener("click",()=>d(-1)),c.addEventListener("click",()=>d(1)),a();
+//# sourceMappingURL=index.78945eee.js.map
